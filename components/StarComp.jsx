48126049f64e7446caadf5656602783ef0937fdc
Lukas Bolleter <teeloewe@gmail.com>
2024-05-12 22:13:02 +0200

    added starcomp

--- components/StarComp.jsx
+++ components/StarComp.jsx
@@ -1,22 +1,31 @@
 import StarSvg from "./StarSvg";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState, useRef } from "react";
 
 export default function StarComp() {
     const [stars, setStars] = useState(0)
+    const divRef = useRef(0)
 
+    useEffect(() => {
+        for (let child of divRef.current.children) {
+            child.classList.remove("svg-star-yellow")
+        }
 
+        for (let i = 0; i < stars; i++) {
+            divRef.current.children[i].classList.add("svg-star-yellow")
+        }
+    }, [stars])
 
-    function selectStar(n) {
-        console.log("GAY")
+    function selectStar(n, e) {
+        setStars(n)
     }
 
     return (
-        <div className="flex flex-row">
-            <StarSvg className="svg-star" onClick={selectStar(1)}/>
-            <StarSvg className="svg-star" onClick={selectStar(2)}/>
-            <StarSvg className="svg-star" onClick={selectStar(3)}/>
-            <StarSvg className="svg-star" onClick={selectStar(4)}/>
-            <StarSvg className="svg-star" onClick={selectStar(5)}/>
+        <div ref={divRef} className="flex flex-row">
+            <StarSvg className="svg-star" onClick={(e) => selectStar(1, e)}/>
+            <StarSvg className="svg-star" onClick={(e) => selectStar(2, e)}/>
+            <StarSvg className="svg-star" onClick={(e) => selectStar(3, e)}/>
+            <StarSvg className="svg-star" onClick={(e) => selectStar(4, e)}/>
+            <StarSvg className="svg-star" onClick={(e) => selectStar(5, e)}/>
         </div>
     );
 }
