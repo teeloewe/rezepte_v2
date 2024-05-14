0e739d71fd75a7dab8f359a99bf3e9752c697b24
Lukas Bolleter <teeloewe@gmail.com>
2024-05-14 18:25:55 +0200

    improved create form

--- pages/rezepte/create/index.js
+++ pages/rezepte/create/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     
     return (
         <div className='container'>
-            <Form className='w-5/6 mx-auto'>
+            <Form className='md:w-5/6 mx-auto'>
                 <Form.Group className="p-2" controlId="formName">
                     <Form.Label>Rezept-Name:</Form.Label>
                     <Form.Control type="text" placeholder="Gib Rezept-Name ein!" />
@@ -16,16 +16,20 @@ export default function Home() {
                     <Form.Label>Beschreibung:</Form.Label>
                     <Form.Control as="textarea" placeholder="Gib Beschreibung ein!" />
                 </Form.Group>
-                <Form.Group className='grid grid-cols-3 px-2' controlId='formDetails'>
-                    <Form.Label>Bewertung:</Form.Label>
-                    <Form.Label>Schwierigkeit:</Form.Label>
-                    <Form.Label>Dauer:</Form.Label>
-                </Form.Group>
-                <Form.Group className='grid grid-cols-3 p-2'>
-                    <StarComp />
-                    <StarComp />
-                    <Form.Control type='number' placeholder='Gib Dauer vom Rezept ein!' />
-                </Form.Group>
+                <div className='md:grid md:grid-cols-3 px-2' controlId='formDetails'>
+                    <Form.Group className='flex flex-col'>
+                        <Form.Label>Bewertung:</Form.Label>
+                        <StarComp />
+                    </Form.Group>
+                    <Form.Group className='flex flex-col'>
+                        <Form.Label>Schwierigkeit:</Form.Label>
+                        <StarComp />
+                    </Form.Group>
+                    <Form.Group className='flex flex-col'>
+                        <Form.Label>Dauer:</Form.Label>
+                        <Form.Control className='p-2 h-full min-h-12' type='number' placeholder='Gib Dauer vom Rezept ein!' />
+                    </Form.Group>
+                </div>
                 <Form.Group className='p-2' controlId='formImage'>
                     <Form.Label>Bild:</Form.Label>
                     <Form.Control type='file'/>
