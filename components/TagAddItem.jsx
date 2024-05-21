3364e7b000bd62d70aa70022a6b0b2645c64b3d5
teeloewe <teeloewe@gmail.com>
2024-05-21 15:58:58 +0200

    continued tag add

--- components/TagAddItem.jsx
+++ components/TagAddItem.jsx
@@ -1,10 +1,11 @@
 import Button from "react-bootstrap/Button"
+import MinusSvg from './svg/MinusSvg'
 
 const TagAddItem = ({ tagName }) => {
     return (
-        <div className="tag-add-item my-2" variant={'light'}>
-            {tagName}
-            
+        <div className="tag-add-item my-2 p-0" variant={'light'}>
+            <span className="mx-2">{tagName}</span>
+            <Button variant="secondary" className=""><MinusSvg /></Button>
         </div>
     )
 }
--- components/svg/MinusSvg.jsx
+++ components/svg/MinusSvg.jsx
@@ -1,8 +1,8 @@
 const SvgComponent = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={800}
-    height={800}
+    width={30}
+    height={30}
     fill="none"
     viewBox="0 0 24 24"
     {...props}
