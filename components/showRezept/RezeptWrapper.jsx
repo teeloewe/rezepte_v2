9400b95c6e4f8bdf722047283a87d59d1829dda2
Lukas Bolleter <teeloewe@gmail.com>
2024-05-30 17:13:18 +0200

    finished (cap) rezepte page

--- components/showRezept/RezeptWrapper.jsx
+++ components/showRezept/RezeptWrapper.jsx
@@ -13,43 +13,45 @@ const RezeptWrapper = ({dataRezept}) => {
             <div className="p-2">
                 <h1>{rezept.name}</h1>
             </div>
-            <div className="p-2">   
+            {/* <div className="p-2 py-3">   
                 {<Image src={bolo} alt="suck"/>}
-            </div>
-            <div className="p-2 rezept-details">
-                <div>
+            </div> */}
+
+            <div className="p-2 rezept-wrapper">
+                <div className="rezept-details">
                     <div>
-                        <span>Bewertung:</span>
+                        <span><b>Bewertung</b></span>
                         <StarCompLite stars={rezept.rating}/>
                     </div>
                     <div>
-                        <span>Schwierigkeit:</span>
+                        <span><b>Schwierigkeit</b></span>
                         <StarCompLite stars={rezept.difficulty} />
                     </div>
                     <span>Dauer: <b>{rezept.duration}</b> Minuten</span>
+                    <div className="rezept-tags">
+                        {rezept.tags.map(t => { return (
+                            <span className="bg-gray-300 px-1 py-0.5 rounded" key={t.name}>{t.name}</span>
+                        )})}
+                    </div>
+                    <div className="flex flex-col">
+                        <span><b>Zutaten</b></span>
+                        {rezept.zutaten.map(z => { return (
+                            <div className="rezept-zutaten" key={z.zutat.name}>
+                                <span className="lg:text-right">{z.quantity}</span>
+                                <span>{z.einheit.name}</span>
+                                <span>{z.zutat.name}</span>
+                            </div>
+                        )})}
+                    </div>
                 </div>
-            </div>
-            <div className="p-2 rezept-tags">
-                {rezept.tags.map(t => { return (
-                    <span className="bg-gray-300 px-1 py-0.5 rounded" key={t.name}>{t.name}</span>
-                )})}
-            </div>
-            <div className="p-2 rezept-anleitung">
-                <div className="flex flex-col">
-                    <span><b>Zutaten:</b></span>
-                    {rezept.zutaten.map(z => { return (
-                        <div className="rezept-zutaten" key={z.zutat.name}>
-                            <span className="lg:text-right">{z.quantity}</span>
-                            <span>{z.einheit.name}</span>
-                            <span>{z.zutat.name}</span>
-                        </div>
-                    )})}
-                </div>
-                <div>
-                    <span><b>Beschreibung:</b></span>
+                
+                <div className="rezept-anleitung">
+                    <span><b>Beschreibung</b></span>
                     <div>{rezept.description}</div>
                 </div>
             </div>
+            
+            <hr></hr>
             <div>
                 <span className="block text-right">Hinzugefügt am: {dateFormatter.format(new Date(rezept.createdAt))}</span>
                 <span className="block text-right">Geändert am: {dateFormatter.format(new Date(rezept.updatedAt))}</span>
